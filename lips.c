2c1790662bedfb56e5b5ce6db7f138cb9a7d7848
Martin Olsen <m@rtinolsen.net>
2012-04-29 15:10:20 +0200

    read repl.lips, execute.

--- lips.c
+++ lips.c
@@ -1,15 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "repl.h"
+#include "lisp.h"
+#include "lisp_eval.h"
+#include "lisp_print.h"
 
 int main(void) {
-    fprintf(stderr, "       LISP  Interactive  Performance  System\n");
+    FILE *repl_file = fopen("repl.lips", "r");
 
-    repl_t *repl = repl_init("LIPS> ");
+    if(repl_file == NULL) {
+        perror("fopen");
+        exit(EXIT_FAILURE);
+    }
 
-    repl_run(repl);
-    repl_destroy(repl);
+    fseek(repl_file, 0L, SEEK_END);
+    size_t bufsz = ftell(repl_file);
+
+    fseek(repl_file, 0L, SEEK_SET);
+
+    char *buf = (char *) calloc(bufsz + 1, sizeof(char));
+
+    if(buf == NULL) {
+        perror("calloc");
+        exit(EXIT_FAILURE);
+    }
+
+    fread(buf, sizeof(char), bufsz, repl_file);
+    fclose(repl_file);
+
+    lisp_t *lisp = lisp_new();
+
+    size_t i = 0;
+    char pprev = 0, prev = 0;
+
+    while(i < bufsz) {
+        if(prev == 0 || (pprev == '\n' && prev == '\n' && buf[i] != '\n'))
+            lisp_eval(lisp, lisp_read(lisp, buf + i, bufsz - i));
+
+        pprev = prev;
+        prev = buf[i++];
+    }
+
+    free(buf);
 
     exit(EXIT_SUCCESS);
 }
